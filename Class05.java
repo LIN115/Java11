c2e2b89b24947d05776c75c4cfd30d9e9372d5bf
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-12-17 18:53:22 +0800

    Update Class05.java

--- Class05.java
+++ Class05.java
@@ -1,14 +1,15 @@
 package Java11;
+
 interface Math {
 	public void show();
 
-	public abstract void add(int a, int b);
+	public void add(int a, int b);
 
-	public abstract void sub(int a, int b);
+	public void sub(int a, int b);
 
-	public abstract void mul(int a, int b);
+	public void mul(int a, int b);
 
-	public abstract void div(int a, int b);
+	public void div(int a, int b);
 }
 
 class Compute implements Math {
@@ -33,16 +34,13 @@ class Compute implements Math {
 	public void show() {
 		System.out.println("ans=" + ans);
 	}
-
 }
 
 public class Class05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Compute cmp = new Compute();
 		cmp.mul(3, 5);
 		cmp.show();
 	}
-
 }
